334b5e28ef3e133ed92397007f0fba9c3794bb8d
vlescot <vincent.lescot@gmail.com>
2018-03-20 13:42:52 +0100

    Views for the administrable and public spaces. Running with self routing at index.php which are just to pratice #2

---
+++ App/View/Front/layout.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+  
+    <title>Blog</title>
+    
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="./../../../Public/img/favicon.ico">
+  
+    <!-- Bootstrap core CSS -->
+    <link href="./../../../Public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  
+    <!-- Custom fonts for this template -->
+    <link href="./../../../Public/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  
+    <!-- Plugin CSS -->
+    <link href="./../../../Public/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+  
+    <!-- Custom styles for this template -->
+    <link href="./../../../Public/css/creative.css" rel="stylesheet">
+
+    <!-- My styles -->
+    <link href="./../../../Public/css/front_style.css" rel="stylesheet">
+  
+  </head>
+
+  <body id="front-app">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="index.php">Vincent Lescot</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="index.php">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="index.php?view=blog">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="index.php#contact">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="./../Back/">ADMIN</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+<?= $content ?>
+
+    <footer>
+        <div class="row" id="my-links">
+            <a class="offset-md-2 col-md-2 col-6" href="https://github.com/vlescot" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+            <a class="col-md-2 col-6" href="https://www.linkedin.com/in/vincent-lescot-70916959/" target="_blank"><i class="fab fa-linkedin-in fa-2x"></i></a>
+            <a class="col-md-2 col-6" href="https://twitter.com/VinceLesc" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+            <a class="col-md-2 col-6" href="mailto:vincent.lescot@gmail.com" target="_blank"><i class="far fa-envelope fa-2x"></i></a>
+        </div>
+        <div id="copyright" class='text-center'>© 2018 - Réalisé par Vincent Lescot</div>
+    </footer>
+    
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="./../../../Public/vendor/jquery/jquery.min.js"></script>
+    <script src="./../../../Public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="./../../../Public/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="./../../../Public/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="./../../../Public/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="./../../../Public/js/creative.js"></script>
+  </body>
+</html>
