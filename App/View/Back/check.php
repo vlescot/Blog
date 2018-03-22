3f5e8fbff49b81a079d891e457f55f1cf9242945
vlescot <vincent.lescot@gmail.com>
2018-03-22 15:27:14 +0100

    UML Diagrams creation #11

--- App/View/Back/check.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (isset($_POST)) {
-	echo("<pre>");
-	print_r($_POST);
-}
--- Public/js/script.js
+++
@@ -1,8 +0,0 @@
-$("document").ready(function(){
-	var bodyHeight = Math.round($("body").height());
-	var windowHeight = Math.round($(window).height());
-	
-	if (windowHeight - bodyHeight > 0) {
-		$("footer").css("margin-top", windowHeight - bodyHeight - 74 )
-	}
-});
